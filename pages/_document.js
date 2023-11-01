c1852702739303ac8b7f24d780c057420791dc80
Trisna Chandra Laksana <109122013+Trisnavhi@users.noreply.github.com>
2023-11-01 08:41:23 +0700

    fix: remove import

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
@@ -14,18 +13,6 @@ export default function Document() {
 height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
         />
-        <Script id="my-script-hotjar-gr">
-          {`
-            (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:3683651,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          `}
-        </Script>
       </body>
     </Html>
   );
